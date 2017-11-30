5821d3b84965fd4d846a9e49cc2bba5625025eb1
Shrisom Laha <ShrisomL@hexaware.com>
2017-11-30 17:23:17 +0530

    Java restservice file is modified

--- restservice/leavemanager/src/main/java/com/hexaware/ftp01/model/Employee.java
+++ restservice/leavemanager/src/main/java/com/hexaware/ftp01/model/Employee.java
@@ -14,8 +14,16 @@ public class Employee {
 
   /**
    * empId to store employee id.
+     empName to store employee name.
+     empPhone to store employee phone number.
+     empEmail to store employee email.
+     empDept to store employee department.
+     empManagerId to store manager id.
+     empLeaveBalance to store employee leave balance.
+     empDoj to store employee date of joining.
    */
-  private int empId;
+  private int empId,empPhone,empManagerId,empLeaveBalance;
+  private String empName,empEmail,empDept,empDoj;
 
   @Override
   public final boolean equals(final Object obj) {
@@ -54,12 +62,123 @@ public class Employee {
 
   /**
    *
+   * @param argEmpName to set employee name.
+   */
+  public final void setEmpName(final int argEmpName) {
+    this.empName = argEmpName;
+  }
+
+   /**
+   * Gets the EmployeeName.
+   * @return this Employee's Name.
+   */
+  public final int getEmpName() {
+    return empName;
+  }
+
+  /**
+   *
+   * @param argEmpPhone to set employee Phone.
+   */
+  public final void setEmpPhone(final int argEmpPhone) {
+    this.empPhone = argEmpPhone;
+  }
+
+ /**
+   * Gets the EmployeePhone.
+   * @return this Employee's Phone.
+   */
+  public final int getEmpPhone() {
+    return empPhone;
+  }
+
+  /**
+   *
+   * @param argEmpEmail to set employee Email.
+   */
+  public final void setEmpEmail(final int argEmpEmail) {
+    this.empEmail = argEmpEmail;
+  }
+
+   /**
+   * Gets the EmployeeEmail.
+   * @return this Employee's email.
+   */
+  public final int getEmpEmail() {
+    return empEmail;
+  }
+
+  /**
+   *
+   * @param argEmpDept to set employee Dept.
+   */
+  public final void setEmpDept(final int argEmpDept) {
+    this.empDept = argEmpDept;
+  }
+
+   /**
+   * Gets the EmployeeDept.
+   * @return this Employee's Dept.
+   */
+  public final int getEmpDept() {
+    return empDept;
+  }
+
+  /**
+   *
+   * @param argEmpManagerId to set employee ManagerId.
+   */
+  public final void setEmpManagerId(final int argEmpManagerId) {
+    this.empManagerId = argEmpManagerId;
+  }
+
+   /**
+   * Gets the EmployeeManagerId.
+   * @return this Employee's ManagerId.
+   */
+  public final int getEmpManagerId() {
+    return empManagerId;
+  }
+
+  /**
+   *
+   * @param argEmpLeaveBalance to set employee LeaveBalance.
+   */
+  public final void setEmpLeaveBalance(final int argEmpLeaveBalance) {
+    this.empLeaveBalance = argEmpLeaveBalance;
+  }
+
+   /**
+   * Gets the EmployeeLeaveBalance.
+   * @return this Employee's LeaveBalance.
+   */
+  public final int getEmpLeaveBalance() {
+    return empLeaveBalance;
+  }
+
+  /**
+   *
+   * @param argEmpDoj to set employee Doj.
+   */
+  public final void setEmpDoj(final int argEmpDoj) {
+    this.empDoj = argEmpDoj;
+  }
+
+   /**
+   * Gets the EmployeeDoj.
+   * @return this Employee's Doj.
+   */
+  public final int getEmpDoj() {
+    return empDoj;
+  }
+
+  /**
+   *
    * @param argEmpId to set employee id.
    */
   public final void setEmpId(final int argEmpId) {
     this.empId = argEmpId;
   }
-
   /**
    * The dao for employee.
    */
