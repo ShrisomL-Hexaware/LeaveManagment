d4bd9668f1c85cb7c72ac51484e8561e109e6606
saumy <SaumyV@hexaware.com>
2017-12-29 09:24:29 +0530

    ApproveDeny Rest

--- restservice/leavemanager/src/main/java/com/hexaware/ftp01/util/LeaveDetailsRest.java
+++ restservice/leavemanager/src/main/java/com/hexaware/ftp01/util/LeaveDetailsRest.java
@@ -3,11 +3,16 @@ package com.hexaware.ftp01.util;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
+import javax.ws.rs.POST;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
 import com.hexaware.ftp01.model.LeaveDetails;
+import com.hexaware.ftp01.model.ManagerResponse;
+import com.hexaware.ftp01.model.LeaveStatus;
+
 
 /**
  * This class provides a REST interface for the leave details entity.
@@ -17,8 +22,8 @@ public class LeaveDetailsRest {
 
   /**
    * Returns a specific leave details's details.
-   * @param id the id of the employee
-   * @return the leave details
+   * @param id the id of the employee.
+   * @return the leave details.
    */
   @GET
   @Path("{id}")
@@ -44,5 +49,20 @@ public class LeaveDetailsRest {
       throw new NotFoundException("No such Employee ID: " + empId);
     }
     return leavedetails;
+
+  /**
+   * Returns a specific leave details's details.
+   * @param leaveId to select leave Id.
+   * @param mr for the manager response for the employee's leave.
+   * @return the leave details.
+   */
+  @POST
+  @Path("{leaveId}/approveDeny")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public final String approveDeny(@PathParam("leaveId") final int leaveId, final ManagerResponse mr) {
+    LeaveDetails leave = LeaveDetails.listById(leaveId);
+    String result = leave.approveDeny(LeaveStatus.valueOf(mr.getStatus()), leaveId, mr.getResponse());
+    return "Manager Response:" + result + "  ";
   }
 }
